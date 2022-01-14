9b1a0aa751931621f49a833e055caff5ffffb2c1
vaibhav0311 <sharma.vaibhav0311@gmail.com>
2022-01-14 22:09:57 +0530

    Added alert for ending the quiz and reset the quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() {
   runApp(MyApp());
@@ -43,22 +44,40 @@ class _QuizAppState extends State<QuizApp> {
 
   void quizCheck(bool userPickedAnswer) {
     bool correctAns = quizbrain.getCorrectAnswer();
-    if (userPickedAnswer == correctAns) {
-      setState(() {
-        quizbrain.nextQuestion();
-        scoreKeeper.add(addIcon(Colors.green));
-      });
-    } else {
-      setState(() {
-        quizbrain.nextQuestion();
-        scoreKeeper.add(addIcon(Colors.red));
-      });
-    }
+    setState(() {
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+      //HINT! Step 4 Part B is in the quiz_brain.dart
+      //TODO: Step 4 Part C - reset the questionNumber,
+      //TODO: Step 4 Part D - empty out the scoreKeeper.
+      if (quizbrain.isFinished()) {
+        Alert(
+                context: context,
+                title: 'End!',
+                desc: 'You have reached the end of the quiz.')
+            .show();
+        quizbrain.reset();
+
+        scoreKeeper = [];
+      } else {
+        if (userPickedAnswer == correctAns) {
+          quizbrain.nextQuestion();
+          scoreKeeper.add(addIcon(Colors.green));
+        } else {
+          quizbrain.nextQuestion();
+          scoreKeeper.add(addIcon(Colors.red));
+        }
+      }
+    });
   }
 
   List<Icon> scoreKeeper = [];
 
   QuizBrain quizbrain = QuizBrain();
+
+  // if(quizbrain.isFinished()==1){
+  // Alert(context: , title: "RFLUTTER", desc: "Flutter is awesome.").show();
+  // }
 
   @override
   Widget build(BuildContext context) {
